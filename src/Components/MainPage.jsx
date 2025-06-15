24a42fc2b27466031f79c495338b701476bc4e70
SharwanKunwar <sharwankunwar07@gmail.com>
2025-06-16 01:16:25 +0545

    updated

--- src/Components/MainPage.jsx
+++ src/Components/MainPage.jsx
@@ -11,7 +11,9 @@ function MainPage() {
         <section id='about' className='bg-blue-300/30 w-screen h-screen flex justify-center items-center'>About page</section>
         <section id='blogs' className='bg-blue-200/30 w-screen h-screen flex justify-center items-center'>Blogs page</section>
         <section id='projects' className='bg-blue-400/30 w-screen h-screen flex justify-center items-center'>Projects page</section>
-        <section id='contact' className='bg-green-400/30 w-screen h-screen flex justify-center items-center'>Contact page</section>
+        <section id='contact' className='bg-blue-400/30 w-screen h-screen flex justify-center items-center'>Contact page</section>
+        <section id='contact' className='bg-blue-400/30 w-screen h-screen flex justify-center items-center'>fotter page</section>
+        
     </>
   )
 }
