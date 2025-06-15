99d1127ec835b90234ee5e783c8ae67b1c0638b0
SharwanKunwar <sharwankunwar07@gmail.com>
2025-06-15 22:38:45 +0545

    updated

--- src/Components/MainPage.jsx
+++ src/Components/MainPage.jsx
@@ -8,15 +8,17 @@ function MainPage() {
   return (
     <>
         <section id='home' style={style01} className='w-screen h-screen flex justify-center items-center'>
-            page01 
+            Home page
             {/* here */}
-            <div className='bg-red-400 w-[300px] h-[300px]'></div>
+            {/* <div className='bg-red-400 w-[300px] h-[300px]'></div> */}
         </section>
-        <section id='about' className='bg-blue-400/30 w-screen h-screen flex justify-center items-center'>
-            page02
+        <section id='about' className='bg-blue-300/30 w-screen h-screen flex justify-center items-center'>
+            About page
         </section>
+        <section id='blogs' className='bg-blue-200/30 w-screen h-screen flex justify-center items-center'>Blogs page</section>
+        <section id='projects' className='bg-blue-400/30 w-screen h-screen flex justify-center items-center'>Projects page</section>
         <section id='contact' className='bg-green-400/30 w-screen h-screen flex justify-center items-center'>
-            page03
+            Contact page
         </section>
     </>
   )
