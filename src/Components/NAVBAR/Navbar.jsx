cef6525d9b0b0f7128d490d49179da8ad321bf50
SharwanKunwar <sharwankunwar07@gmail.com>
2025-06-15 11:03:29 +0545

    updated

--- src/Components/NAVBAR/Navbar.jsx
+++ src/Components/NAVBAR/Navbar.jsx
@@ -5,9 +5,9 @@ import { easeInOut, motion } from 'motion/react';
 
 function Navbar() {
   return (
-    <div className='absolute bg-gray-50/30 drop-shadow-2xl w-screen md:h-[15%] h-[100px] flex justify-center md:items-center z-50'>
+    <div className='absolute bg-gray-50/30 backdrop-blur-2xl w-screen md:h-[15%] h-[100px] flex justify-center md:items-center z-50'>
 
-      <nav className='bg-white/30 drop-shadow-2xl md:w-[90%] w-full md:h-[70%] h-[90%] md:rounded-full'>
+      <nav className='md:bg-white/30 md:backdrop-blur-2xl md:w-[90%] w-full md:h-[70%] h-[90%] md:rounded-full'>
         
       </nav>
 
