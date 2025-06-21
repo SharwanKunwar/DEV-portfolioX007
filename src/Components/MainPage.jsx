a702ea2c9d47297479629f92346b6cf4457e6c4b
SharwanKunwar <sharwankunwar07@gmail.com>
2025-06-21 15:47:57 +0545

    updated

--- src/Components/MainPage.jsx
+++ src/Components/MainPage.jsx
@@ -5,9 +5,12 @@ import { ShootingStars } from './ui/shooting-stars';
 import { PointerHighlightDemo } from './ui/PointerHighlightDemo';
 import { motion } from 'motion/react';
 import { useEffect, useState } from 'react';
+import {  X } from 'lucide-react';
+
 
 function MainPage() {
   const [xDistance, setXDistance] = useState(-1500);
+  const [LearnMore, setLearnMore] = useState(false);
 
   useEffect(() => {
     const updateXDistance = () => {
@@ -90,7 +93,7 @@ function MainPage() {
               whileInView={{ x: 0, opacity: 1, filter: 'blur(0px)' }}
               transition={{ duration: 0.5, ease: 'easeOut' }}
               viewport={{ once: true, amount: 0.3 }}
-              className="w-[90%] h-[90%] relative"
+              className="w-[90%] h-[90%] relative "
             >
               <h1 className="text-2xl font-bold text-neutral-300">
                 Full Stack Developer
@@ -102,12 +105,124 @@ function MainPage() {
               </p>
               <section className="flex justify-start">
                 <button
+                onClick={(e)=>setLearnMore(!LearnMore)}
                   type="button"
-                  className=" w-[150px] h-[40px] rounded-sm text-sky-400 text-start"
+                  className=" w-[150px] h-[40px] rounded-sm text-sky-400 text-start md:hidden"
                 >
                   Learn More
                 </button>
+                <br />
+               {LearnMore && (
+                  <div className='bg-black/80 fixed inset-0 mt-13 z-50 flex justify-center items-center md:hidden'>
+                    <div className='bg-white w-[100%] h-[100%] rounded-xl p-5 overflow-y-auto'>
+                      <section className='justify-end flex'>
+                        <X onClick={() => setLearnMore(false)}
+                        className='text-sm text-black underline ' size={30}/>
+                     
+                      </section>
+                      <div className="space-y-6">
+                        <div className="grid grid-cols-1 gap-y-4">
+                          {/* Full Name */}
+                          <div>
+                            <p className="text-gray-500">Full Name</p>
+                            <p className="font-semibold text-lg">Sharwan jung kunwar</p>
+                          </div>
+
+                          {/* Phone */}
+                          <div>
+                            <p className="text-gray-500">Phone</p>
+                            <p className="font-semibold text-blue-700">+9779763290022</p>
+                          </div>
+
+                          {/* Age */}
+                          <div>
+                            <p className="text-gray-500">Age</p>
+                            <p className="font-semibold text-lg">21 Years</p>
+                          </div>
+
+                          {/* Email */}
+                          <div>
+                            <p className="text-gray-500">Email</p>
+                            <p className="font-semibold text-lg break-words">sharwankunwar07@gmail.com</p>
+                          </div>
+
+                          {/* Occupation */}
+                          <div>
+                            <p className="text-gray-500">Occupation</p>
+                            <p className="font-semibold text-lg">Software Developer</p>
+                          </div>
+
+                          {/* Nationality */}
+                          <div>
+                            <p className="text-gray-500">Nationality</p>
+                            <p className="font-semibold text-lg">Nepali</p>
+                          </div>
+                          {/* address */}
+                          <div>
+                            <p className="text-gray-500">Address</p>
+                            <p className="font-semibold text-lg">Attraiya-Kailali</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                )}
+
+
               </section>
+                <br />
+                <p className='md:flex hidden text-start text-gray-50'>🚀 "From interactive frontends to robust backends—crafting full-stack experiences that inspire."</p>
+              <section className=' md:flex hidden w-full h-[50%] justify-start items-center'>
+                  <div className=' w-[100%] h-[90%] flex justify-center items-center'>
+                    <div class="w-full mx-auto bg-white shadow-md rounded-xl p-6 md:p-8 space-y-6">
+                          <div class="grid grid-cols-1 sm:grid-cols-2 gap-y-6 gap-x-12 ">
+                            
+                            {/* <!-- Full Name --> */}
+                            <div>
+                              <p class="text-gray-500">Full Name</p>
+                              <p class="font-semibold text-lg">Sharwan jung kunwar</p>
+                            </div>
+
+                            {/* <!-- Phone --> */}
+                            <div>
+                              <p class="text-gray-500">Phone</p>
+                              <p class="font-semibold text-blue-700">+9779763290022</p>
+                            </div>
+
+                            {/* <!-- Age --> */}
+                            <div>
+                              <p class="text-gray-500">Age</p>
+                              <p class="font-semibold text-lg">21 Years</p>
+                            </div>
+
+                            {/* <!-- Email --> */}
+                            <div>
+                              <p class="text-gray-500">Email</p>
+                              <p class="font-semibold text-lg">sharwankunwar07@gmail.com</p>
+                            </div>
+
+                            {/* <!-- Occupation --> */}
+                            <div>
+                              <p class="text-gray-500">Occupation</p>
+                              <p class="font-semibold text-lg">Software Developer</p>
+                            </div>
+
+                            {/* <!-- Nationality --> */}
+                            <div>
+                              <p class="text-gray-500">Nationality</p>
+                              <p class="font-semibold text-lg">Nepali</p>
+                            </div>
+
+                          </div>
+                        </div>
+
+                  </div>
+              </section>
+              <section className=' w-full h-[10%] text-gray-50 md:flex hidden'>
+                <h1>Sharwan jung kunwar</h1>
+                <p>Currently live in Bhaktapur-Bode, from Far west side</p>
+              </section>
+              
             </motion.div>
           </div>
         </div>
